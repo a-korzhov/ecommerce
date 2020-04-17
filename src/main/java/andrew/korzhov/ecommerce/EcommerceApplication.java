465fa3033afa54f1b480e8ba08ef06c5888c0623
Andrew Korzhov <korzh17121996@gmail.com>
2020-04-17 20:48:19 +0200

    Cleanup

--- src/main/java/andrew/korzhov/ecommerce/EcommerceApplication.java
+++ src/main/java/andrew/korzhov/ecommerce/EcommerceApplication.java
@@ -2,9 +2,8 @@ package andrew.korzhov.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class EcommerceApplication {
 
     public static void main(String[] args) {
